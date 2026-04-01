0a6966efb23a96fdb566db96baf91bcbd4157d54
jayagrawal-petabyte <jayagrawal72511@gmail.com>
2026-04-01 21:27:49 +0530

    UC1: Add welcome message module

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,2 +1,9 @@
 public class BookMyStay {
+    public static void main(String[] args) {
+        // Display welcome message
+        System.out.println("=====================================");
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("=====================================");
+    }
 }
