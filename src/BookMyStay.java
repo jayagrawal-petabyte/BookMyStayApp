783f55b68243299126d3daec6036cd42e02de7aa
jayagrawal-petabyte <jayagrawal72511@gmail.com>
2026-04-01 21:41:24 +0530

    UC2: Hardcoded palindrome check

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,9 +1,69 @@
 public class BookMyStay {
+
     public static void main(String[] args) {
-        // Display welcome message
-        System.out.println("=====================================");
-        System.out.println("Welcome to Book My Stay App");
-        System.out.println("Hotel Booking System v1.0");
-        System.out.println("=====================================");
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        int singleRoomAvailable = 5;
+        int doubleRoomAvailable = 3;
+        int suiteRoomAvailable = 2;
+
+        System.out.println("Hotel Room Initialization");
+        System.out.println();
+
+        System.out.println("Single Room:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleRoomAvailable);
+        System.out.println();
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleRoomAvailable);
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteRoomAvailable);
+    }
+
+    public abstract class Room {
+
+        protected int numberOfBeds;
+        protected int squareFeet;
+        protected double pricePerNight;
+
+        public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+            this.numberOfBeds = numberOfBeds;
+            this.squareFeet = squareFeet;
+            this.pricePerNight = pricePerNight;
+        }
+
+        public void displayRoomDetails() {
+            System.out.println("Beds: " + numberOfBeds);
+            System.out.println("Size: " + squareFeet + " sqft");
+            System.out.println("Price per night: " + pricePerNight);
+        }
+    }
+
+    public class SingleRoom extends Room {
+
+        public SingleRoom() {
+            super(1, 250, 1500.0);
+        }
+    }
+
+    public class DoubleRoom extends Room {
+
+        public DoubleRoom() {
+            super(2, 400, 2500.0);
+        }
+    }
+
+    public class SuiteRoom extends Room {
+
+        public SuiteRoom() {
+            super(3, 750, 5000.0);
+        }
     }
 }
